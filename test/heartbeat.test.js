88d66b07dee81bf653eb38d8ed76bfbe53a520fb
István Kárász <ikarasz@outlook.com>
2019-11-28 11:55:36 +0800

    test(heartbeat): add missing test for unavailable db

--- test/heartbeat.test.js
+++ test/heartbeat.test.js
@@ -1,4 +1,6 @@
 import request from 'supertest';
+import sinon from 'sinon';
+import db from '../db/models/index.js';
 import app from './setup';
 
 describe('/heartbeat with working ENV vars', () => {
@@ -17,6 +19,19 @@ describe('/heartbeat with working ENV vars', () => {
       .expect(200, {
         db: 1,
       }, done);
+  });
+
+  it('[GET] should feedback if storage is not accessible', (done) => {
+    sinon.stub(db.sequelize, 'authenticate').rejects();
+    request(app)
+      .get('/heartbeat')
+      .expect('Content-Type', /json/)
+      .expect(200, {
+        db: 0,
+      }, (...args) => {
+        db.sequelize.authenticate.restore();
+        done(...args);
+      });
   });
 
   it('Unsupported method should send 405', (done) => {
