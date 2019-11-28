bde43cfaec11979504a085563948f607176e28a9
István Kárász <ikarasz@outlook.com>
2019-11-28 21:01:09 +0800

    refactor(tests): rename test cases

--- test/heartbeat.test.js
+++ test/heartbeat.test.js
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 import db from '../db/models/index.js';
 import app from './setup';
 
-describe('/heartbeat with working ENV vars', () => {
+describe('[GET] /heartbeat with working ENV vars', () => {
   let server;
 
   beforeEach((done) => {
@@ -12,7 +12,7 @@ describe('/heartbeat with working ENV vars', () => {
 
   afterEach(() => server.close());
 
-  it('[GET] should confirm the db connection', (done) => {
+  it('should confirm the db connection', (done) => {
     request(app)
       .get('/heartbeat')
       .expect('Content-Type', /json/)
@@ -21,7 +21,7 @@ describe('/heartbeat with working ENV vars', () => {
       }, done);
   });
 
-  it('[GET] should feedback if storage is not accessible', (done) => {
+  it('should feedback if storage is not accessible', (done) => {
     sinon.stub(db.sequelize, 'authenticate').rejects();
     request(app)
       .get('/heartbeat')
@@ -34,7 +34,7 @@ describe('/heartbeat with working ENV vars', () => {
       });
   });
 
-  it('Unsupported method should send 405', (done) => {
+  it('should send 405 dor unsupported methods', (done) => {
     request(app)
       .post('/heartbeat')
       .expect(405, done);
