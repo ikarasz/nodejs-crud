21bec27b19e962a060b48228be4ec27f4fab292b
István Kárász <ikarasz@outlook.com>
2019-11-29 14:29:27 +0800

    chore: fallback to .env if no specific file

--- environment.js
+++ environment.js
@@ -1,9 +1,18 @@
+import fs from 'fs';
 import dotenv from 'dotenv';
 
-const envFile = process.env.NODE_ENV
-  ? `.env.${process.env.NODE_ENV}`
-  : '.env';
+const nodeEnv = process.env.NODE_ENV;
+let configFile;
+
+try {
+  fs.accessSync(`.env.${nodeEnv}`);
+  configFile = `.env.${nodeEnv}`;
+} catch (err) {
+  configFile = '.env';
+}
+
+console.log('Using env file: ', configFile);
 
 dotenv.config({
-  path: envFile,
+  path: configFile,
 });
